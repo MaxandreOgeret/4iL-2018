690cf35ec5946f55f46770c7ccc09db6c3292bbe
root <root@vps493353.ovh.net>
2017-12-17 19:11:25 +0100

    first commit

---
+++ src/AppBundle/Controller/ArticleController.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mackou
+ * Date: 23/09/2017
+ * Time: 18:07
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Article;
+use AppBundle\Form\ArticleType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Filesystem\Filesystem;
+
+class ArticleController extends Controller
+{
+    public function identityChecker()
+    {
+        $securityContext = $this->container->get('security.authorization_checker');
+        if (!$securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw new AccessDeniedException();
+        }
+    }
+
+    /**
+     * @Route("getarticles", name="getarticles")
+     */
+    public function getAllAction() {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $articles = $em->getRepository('AppBundle:Article')->getArticles();
+        $toRender = "";
+
+        $soutiens = $this->render(':elements:help.html.twig')->getContent();
+
+        foreach ($articles as $article) {
+            /** @var DateTime $date */
+            $date = $article['date'];
+            $render = $this->render(
+                ':elements:card.html.twig',
+                [
+                    'text' => $article['text'],
+                    'title' => $article['title'],
+                    'id' => $article['id'],
+                    'image' => $article['image'],
+                    'date' => $date->format('d/m/Y'),
+                ]
+            );
+            $toRender = $toRender."\n".$render->getContent();
+        }
+
+        return new JsonResponse($soutiens.$toRender);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("add", name="add")
+     */
+    public function addAction()
+    {
+        $this->identityChecker();
+
+        $article = new Article();
+
+        $form = $this->createForm
+        (
+            ArticleType::class,
+            $article,
+            [
+                'action' => $this->generateUrl('handlearticleform'),
+            ]
+        )
+            ->createView();
+        return $this->render(
+            'add/addpage.html.twig',
+            [
+                'form' => $form,
+                'action' => 'Création',
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param Article|null $article
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("handle-article-form/{article}", name="handlearticleform", )
+     */
+    public function handleFormAction(Request $request, Article $article = null)
+    {
+        $this->identityChecker();
+
+        if (is_null($article)) {
+            $article = new Article();
+        }
+
+        $form = $this->createForm
+        (
+            ArticleType::class,
+            $article,
+            [
+                'action' => $this->generateUrl('handlearticleform'),
+            ]
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $time = new \DateTime('now');
+
+            if (!is_null($article->getImagePath())) {
+                /** @var UploadedFile $image */
+                $image = $article->getImagePath();
+                $size = $image->getSize();
+                $ext = $image->guessExtension();
+
+                //server-side controls - EXTENSION AND SIZE
+                if (($size <= Article::MAXIMGSIZE*1000) && (in_array($ext, Article::IMGEXT))) {
+                    $fileName = md5(uniqid()).'.'.$ext;
+                    $image->move(
+                        $this->getParameter('image_directory'),
+                        $fileName
+                    );
+                    $article->setImage($fileName);
+                } else {
+                    dump($size <= Article::MAXIMGSIZE*100);
+                    dump(in_array($ext, Article::IMGEXT));
+                    $article->setImage(null);
+                    $article->setImagePath(null);
+                }
+            }
+
+            $article->setDate($time);
+            $article->setText($article->getText());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('homepage'));
+        } else {
+            throw new Exception('Form is not valid or not submitted');
+        }
+    }
+
+    /**
+     * @param $article Article
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("edit/{id}", name="editarticle")
+     */
+    public function editArticleAcion(Article $article)
+    {
+        $this->identityChecker();
+
+        $form = $this->createForm
+        (
+            ArticleType::class,
+            $article,
+            [
+                'action' => $this->generateUrl('handlearticleform', ['article'=>$article->getId()]),
+            ]
+        );
+
+        return $this->render(
+            'add/addpage.html.twig',
+            [
+                'form' => $form->createView(),
+                'action' => 'Edition'
+            ]
+        );
+    }
+
+    /**
+     * @param Article $article
+     * @return JsonResponse
+     * @Route("/del/{id}", name="delarticle", defaults={"id"=null}, condition="request.isXmlHttpRequest()")
+     */
+    public function delArticleAction(Article $article=null)
+    {
+        $this->identityChecker();
+
+        $fs = new Filesystem();
+        $fs->remove([$this->getParameter('image_directory').$article->getImage()]);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
+        return new JsonResponse('ok');
+    }
+
+    /**
+     * @param Article $article
+     * @return JsonResponse
+     * @Route("/delimg/{id}", name="delimg", condition="request.isXmlHttpRequest()")
+     */
+    public function delImageAction(Article $article)
+    {
+        $this->identityChecker();
+
+        $image = $article->getImage();
+        $fs = new Filesystem();
+        $fs->remove([$this->getParameter('image_directory').$image]);
+
+        $article->setImagePath(null);
+        $article->setImage(null);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+
+        return new JsonResponse('ok');
+    }
+}
---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="article")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ArticleRepository")
+ */
+class Article
+{
+    const MAXIMGSIZE = 100;
+    const IMGEXT = ['jpeg', 'jpg', 'bmp', 'png'];
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @Assert\File( maxSize = "100k", mimeTypesMessage = "Please upload a valid Image")
+     */
+    private $imagePath;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text", length=10000)
+     */
+    private $text;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Article
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     * @return Article
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     * @return Article
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param DateTime $date
+     * @return Article
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImagePath()
+    {
+        return $this->imagePath;
+    }
+
+    /**
+     * @param string $imagePath
+     * @return Article
+     */
+    public function setImagePath($imagePath)
+    {
+        $this->imagePath = $imagePath;
+        return $this;
+    }
+}
+
